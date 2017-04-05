807117bd515bbc71543c0526f3168bf7f278fb99
jeyhoon <jeyechoo@gmail.com>
2017-04-05 17:13:22 +0900

    Initial commit

---
+++ app/src/main/java/com/jey_dev/lib/jeybasedlibrary/dialog/JDialogInterface.java
@@ -0,0 +1,22 @@
+package com.jey_dev.lib.jeybasedlibrary.dialog;
+
+import android.view.View;
+
+/**
+ * Created by jeyhoon on 15. 12. 16..
+ */
+public class JDialogInterface {
+    public interface OnMenuClickListener{
+        public void onClick(JBaseDialog d, View v, int position);
+    }
+
+    public interface OnButtonClickListener {
+        public void onClick(JBaseDialog d, View v);
+    }
+    public interface OnEditConfirmListener{
+        void onConfirm(final JBaseDialog d, final String str, final View v);
+    }
+    public interface OnLocationConfirmListener{
+        void onConfirm(JBaseDialog d, View v, String locationCode, String locationName);
+    }
+}
